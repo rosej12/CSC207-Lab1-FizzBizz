0a745235349b674cc8163b9f81c394a0073c8827
jiangros <rfh.jiang@mail.utoronto.ca>
2024-09-09 18:21:40 -0400

    Task 4 and 5

---
+++ src/Reduce.java
@@ -0,0 +1,15 @@
+public class Reduce {
+    public static void main(String[] args) {
+        int counter = 0;
+        int n = 100;
+        while (n > 0) {
+            if (n % 2 == 0) {
+                n /= 2;
+            } else {
+                n -= 1;
+            }
+            counter++;
+        }
+        System.out.println(counter);
+    }
+}
