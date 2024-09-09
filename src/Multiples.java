beea90b4117d4ac1dc8e1d5c1d518eaa5b394d37
jiangros <rfh.jiang@mail.utoronto.ca>
2024-09-09 18:31:48 -0400

    Multiples includes 0

--- src/Multiples.java
+++ src/Multiples.java
@@ -1,7 +1,7 @@
 public class Multiples {
     public static void main(String[] args) {
         int counter = 0;
-        for (int i = 1; i < 1000; i++) {
+        for (int i = 0; i < 1000; i++) {
             if (i % 3 == 0 || i % 5 == 0) {
                 counter++;
             }
